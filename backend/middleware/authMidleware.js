0fc41f12dd95a4085e7c8ba30f93bf444d6a57a9
Anas <domsayed300@gmail.com>
2022-11-08 14:50:46 +0530

    prepared for deploy

--- backend/middleware/authMidleware.js
+++ backend/middleware/authMidleware.js
@@ -1,5 +1,5 @@
 import asyncHandler from 'express-async-handler';
-import jwt from 'jsonwebtoken';
+import jwt from         'jsonwebtoken';
 
 import User from '../models/userModel.js';
 
@@ -11,7 +11,7 @@ const protect = asyncHandler(async (req,res,next) => {
     ){
         try {
             token=req.headers.authorization.split(' ')[1];
-            const decoded=jwt.verify(token,process.env.jWT_SECRET);
+            const decoded=jwt.verify(token,process.env.JWT_SECRET);
             req.user = await User.findById(decoded.id).select('-password')
             next();
         } catch (error) {
